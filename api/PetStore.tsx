4f3b5c4aca3bd0330e1c1db0da44c34e84fbe187
Axel <selii93@gmail.com>
2021-04-01 13:08:22 +0000

    fetching pets in chunks (#33)

--- api/PetStore.tsx
+++ api/PetStore.tsx
@@ -32,17 +32,38 @@ export const createPet = (user: User, pet: Pet) => {
 export const getMyPets = async (user: User) => {
   const db = firebase.firestore();
   if (user.pets) {
-    var petsRef = db
-      .collection("pets")
-      .where("id", "in", user.pets)
-      .get()
-      .then((snapshot) => {
-        let pets: Pet[] = [];
-        snapshot.forEach((doc) => {
-          pets.push(doc.data());
+    // Firebase only allows to query for 10 items with the `with - in` clause.
+    // To get around that we split the pets id into chunks of 10 and then combine
+    // the results at the end
+    const petChunks = chunk(user.pets, 10);
+    const promises = petChunks.map((petChunk: string[]) => {
+      return db
+        .collection("pets")
+        .where("id", "in", petChunk)
+        .get()
+        .then((snapshot) => {
+          let pets: Pet[] = [];
+          snapshot.forEach((doc) => {
+            pets.push(doc.data());
+          });
+          return pets;
         });
-        return pets;
-      });
-    return await petsRef;
+    });
+    const allPets = await Promise.all(promises);
+    return allPets.flat();
   } else return [];
 };
+
+/**
+ * A function that splits an array into chunks with the maximum size of "size"
+ * Example: chunk([1,2,3,4,5], 2) => [[1,2], [3,4], [5]]
+ */
+function chunk(array: any[], size: number) {
+  const chunked_arr = [];
+  let index = 0;
+  while (index < array.length) {
+    chunked_arr.push(array.slice(index, size + index));
+    index += size;
+  }
+  return chunked_arr;
+}
